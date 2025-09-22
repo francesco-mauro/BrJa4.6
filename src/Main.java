900ce79903f9b173273e81bd22e9911663a1e6a5
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-22 16:46:26 +0200

    RPS loop

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,59 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
         // Declare Variable
 
         Scanner scanner = new Scanner(System.in);
-        String[] choices = {"rock" , "paper", "scissors"};
+        String[] choices = {"sasso", "carta", "forbice"};
         String playerChoice;
         String computerChoice;
-        String playAgain;
+        String playAgain = "yes";
 
-// Get choice from the user
-        System.out.println("Benvenuto! scegli: CARTA SASSO o FORBICE");
-        playerChoice = scanner.nextLine().toLowerCase().trim();
+        do {
 
-        if (!playerChoice.equals("sasso") &&
-                !playerChoice.equals("carta") &&
-                !playerChoice.equals("forbice")){
+            // Get choice from the user
+            System.out.println("Benvenuto! scegli: CARTA SASSO o FORBICE");
+            playerChoice = scanner.nextLine().toLowerCase().trim();
 
-            System.out.println("Devi inserire una scelta valida");
-        }
+            if (!playerChoice.equals("sasso") &&
+                    !playerChoice.equals("carta") &&
+                    !playerChoice.equals("forbice")) {
+
+                System.out.println("Devi inserire una scelta valida");
+                continue;
+            }
+
+            // Get random choice for the computer
+
+            Random random = new Random();
+            computerChoice = choices[random.nextInt(3)].toLowerCase().trim();
+            System.out.println("Computer choice: " + computerChoice);
+
+            // Check WIn conditions
+
+            if (playerChoice.equals(computerChoice)) {
+                System.out.println("PAREGGIO");
+            } else if (
+                    (playerChoice.equals("sasso") && computerChoice.equals("forbice")) ||
+                            (playerChoice.equals("forbice") && computerChoice.equals("carta")) ||
+                            (playerChoice.equals("carta") && computerChoice.equals("sasso"))
+            ) {
+
+                System.out.println("HAI VINTO");
+            } else{
+                System.out.println("HAI PERSO");
+            }
+
+            System.out.println("Gioca ancora? (si/no): ");
+            playAgain = scanner.nextLine().toLowerCase().trim();
+
+        }while (playAgain.equals("si"));
+
+        System.out.println("Grazie di aver giocato!");
 
 
-        // Get random choice for the computer
-        Random random = new Random();
-        // Check WIn conditions
         // Ask to play again
         // Goodbye Message
 
